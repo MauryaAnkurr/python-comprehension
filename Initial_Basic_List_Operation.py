68fd2458743188b98f9f90808d43be9ec61bd647
Ankur Maurya <39486237+MauryaAnkurr@users.noreply.github.com>
2019-07-17 16:23:04 +0530

    First file commit

---
+++ Initial_Basic_List_Operation.py
@@ -0,0 +1,7 @@
+
+
+#Creating a list is as simple as putting different comma-separated values between square brackets. For example −
+
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"]
