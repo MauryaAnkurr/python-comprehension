6fc091d7dfc1c8c9f855e41a64a6deb1ed4d7072
Ankur Maurya <39486237+MauryaAnkurr@users.noreply.github.com>
2019-08-29 12:14:01 +0530

    Update Initial_Basic_List_Operation.py

--- Initial_Basic_List_Operation.py
+++ Initial_Basic_List_Operation.py
@@ -5,3 +5,37 @@
 list1 = ['physics', 'chemistry', 1997, 2000];
 list2 = [1, 2, 3, 4, 5 ];
 list3 = ["a", "b", "c", "d"]
+
+#************************************************************
+#BASIC SYNTAX:
+#[ expression for item in list if conditional ]
+
+#IS EQUIVALENT TO:
+#for item in list:
+#    if conditional:
+#        expression
+
+#************************************************************
+#Another example
+#new_list = []
+#for i in old_list:
+    #if filter(i):
+        #new_list.append(expressions(i))
+
+#List comprehension view .IS EQUIVALENT TO.
+#new_list = [expression(i) for i in old_list if filter(i)]
+
+#*************************************************************
+#Breaking of the statement.How it works  
+# new_list = [expression(i) for i in old_list if filter(i)]
+#1 .new_list    
+#2 .The new list (result).
+#expression(i)
+#Expression is based on the variable used for each element in the old list.
+
+#for i in old_list
+#The word for followed by the variable name to use, followed by the word in the
+#old list.
+
+#if filter(i)
+#Apply a filter with an If-statement.
